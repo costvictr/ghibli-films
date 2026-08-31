92cafcb10a66e7d1cbe9e3cb05550f5f06f0ea4f
costvictr <victrr.cost@gmail.com>
2026-08-31 08:32:36 -0300

    feat: ghibli api

---
+++ src/services/ghibli.ts
@@ -0,0 +1,15 @@
+const BASE_URL = "https://ghibliapi.vercel.app";
+
+interface Film {
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+  director: string;
+}
+
+export const getFilms = async () => {
+  const response = await fetch(`${BASE_URL}/films`);
+  const data = await response.json();
+  return data as Film[];
+};
